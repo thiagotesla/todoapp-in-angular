cd2074c364c7acdd7e9e9186e5c6d5c977f0a69c
Thiago Tesla <thiagobarros95@gmail.com>
2022-03-08 09:46:30 -0300

    adiciona backend fo formulario de tarefa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -9,8 +10,18 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
   public todos: Todo[] = []
   public title: String = 'Minha Lista de Tarefas'
+  public form: FormGroup | any
 
-  constructor(){
+
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(25),
+          Validators.required,
+        ]
+      )]
+    })
     this.todos.push(new Todo(1, 'Passear com o cachorro.', false))
     this.todos.push(new Todo(2, 'Comprar alimento.', false))
     this.todos.push(new Todo(3, 'Não ser calvo.', true))
